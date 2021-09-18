de75e91adec131a7048191675e19ac05557350d0
Thilo Wendt <wendtth65566@th-nuernberg.de>
2021-09-18 18:22:07 +0300

    worked on blog index page

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,8 @@ from django.db.models.fields import Field
 from datetime import date
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.admin.edit_handlers import RichTextField
 from wagtail.core.fields import StreamField
 
@@ -15,6 +16,11 @@ class BlogPost(Page):
     """Page that holds an individual blog post"""
     template ="blog/blog_post.html"
 
+    custom_title = models.CharField(max_length=20,
+        blank=False,
+        null=True, 
+        help_text="This title is displayed on the page")
+    
     author = models.CharField(max_length=50, blank=False, null=True, default="Thilo")
     date = models.DateField("Post date",default=date.today)
     summary = models.TextField(
@@ -22,6 +28,14 @@ class BlogPost(Page):
         null=True,
         help_text='Write a summary of your blog post here',
         max_length=500
+    )
+
+    thumbnail = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+'
     )
 
     content = StreamField(
@@ -33,9 +47,23 @@ class BlogPost(Page):
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel('author'),
-        FieldPanel('date'),
-        FieldPanel('summary'),
+        FieldPanel('custom_title'),
+        MultiFieldPanel(
+            [
+                FieldPanel('summary'),
+                ImageChooserPanel('thumbnail'),
+            ],
+            heading="Thumbnail on Blog Index Page",
+            classname="collapsible collapsed"
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('author'),
+                FieldPanel('date'),
+            ],
+            heading="Meta data of the post",
+            classname="collapsible collapsed"
+        ),
         StreamFieldPanel('content'),
     ]
 
@@ -47,4 +75,33 @@ class BlogPost(Page):
 
 class BlogIndex(Page):
     """Index Page that displays a summary of the blog posts"""
+
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=True,
+        help_text='Overwrites the default title',
+    )
+
+    description = models.TextField(
+        blank=False,
+        null=True,
+        help_text='Write a description of the journey',
+    )
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+        FieldPanel("description"),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+        # Get all posts
+        all_posts = BlogPost.objects.live().public().order_by('-first_published_at')
+
+        context["posts"] = all_posts
+        return context
+
     template ="blog/blog_index.html"
