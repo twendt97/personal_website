69112af445d5c55f578df4f8acc01bc72493ba74
Thilo Wendt <wendtth65566@th-nuernberg.de>
2021-09-26 12:38:37 +0300

    started working on production setup

--- app/thilos_website/settings/production.py
+++ app/thilos_website/settings/production.py
@@ -1,6 +1,11 @@
+from app.thilos_website.settings.dev import SECRET_KEY
 from .base import *
+import os
 
 DEBUG = False
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
 
 try:
     from .local import *
