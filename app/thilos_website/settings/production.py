d3364c38ca91a3b016b2e5feaedf1903f7c40500
Thilo Wendt <wendtth65566@th-nuernberg.de>
2021-10-02 16:50:16 +0300

    production setting runs without nginx locally

--- app/thilos_website/settings/production.py
+++ app/thilos_website/settings/production.py
@@ -1,11 +1,17 @@
-from app.thilos_website.settings.dev import SECRET_KEY
 from .base import *
 import os
 
-DEBUG = False
+DEBUG = True
 
 env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = env['DJANGO_ALLOWED_HOSTS'].split(" ")
+
+COMPRESS_OFFLINE = True
+LIBSASS_OUTPUT_STYLE = 'compressed'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 try:
     from .local import *
